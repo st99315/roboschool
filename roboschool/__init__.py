fb5d2dfe47b6d95349d5d6d9a9d6a60854f2c338
youjun <st99315@yahoo.com.tw>
2017-12-14 22:20:46 +0800

    Adding New Environment
    reacher7d

--- roboschool/__init__.py
+++ roboschool/__init__.py
@@ -6,81 +6,90 @@ register(
     entry_point='roboschool:RoboschoolInvertedPendulum',
     max_episode_steps=1000,
     reward_threshold=950.0,
-    tags={ "pg_complexity": 1*1000000 },
-    )
+    tags={"pg_complexity": 1 * 1000000},
+)
 register(
     id='RoboschoolInvertedPendulumSwingup-v1',
     entry_point='roboschool:RoboschoolInvertedPendulumSwingup',
     max_episode_steps=1000,
     reward_threshold=800.0,
-    tags={ "pg_complexity": 1*1000000 },
-    )
+    tags={"pg_complexity": 1 * 1000000},
+)
 register(
     id='RoboschoolInvertedDoublePendulum-v1',
     entry_point='roboschool:RoboschoolInvertedDoublePendulum',
     max_episode_steps=1000,
     reward_threshold=9100.0,
-    tags={ "pg_complexity": 1*1000000 },
-    )
+    tags={"pg_complexity": 1 * 1000000},
+)
 
 register(
     id='RoboschoolReacher-v1',
     entry_point='roboschool:RoboschoolReacher',
     max_episode_steps=150,
     reward_threshold=18.0,
-    tags={ "pg_complexity": 1*1000000 },
-    )
+    tags={"pg_complexity": 1 * 1000000},
+)
+
+""" Important! """
+register(
+    id='RoboschoolReacher7d-v1',
+    entry_point='roboschool:RoboschoolReacher7d',
+    max_episode_steps=150,
+    reward_threshold=18.0,
+    tags={"pg_complexity": 1 * 1000000},
+)
 
 register(
     id='RoboschoolHopper-v1',
     entry_point='roboschool:RoboschoolHopper',
     max_episode_steps=1000,
     reward_threshold=2500.0,
-    tags={ "pg_complexity": 8*1000000 },
-    )
+    tags={"pg_complexity": 8 * 1000000},
+)
 register(
     id='RoboschoolWalker2d-v1',
     entry_point='roboschool:RoboschoolWalker2d',
     max_episode_steps=1000,
     reward_threshold=2500.0,
-    tags={ "pg_complexity": 8*1000000 },
-    )
+    tags={"pg_complexity": 8 * 1000000},
+)
 register(
     id='RoboschoolHalfCheetah-v1',
     entry_point='roboschool:RoboschoolHalfCheetah',
     max_episode_steps=1000,
     reward_threshold=3000.0,
-    tags={ "pg_complexity": 8*1000000 },
-    )
+    tags={"pg_complexity": 8 * 1000000},
+)
 
 register(
     id='RoboschoolAnt-v1',
     entry_point='roboschool:RoboschoolAnt',
     max_episode_steps=1000,
     reward_threshold=2500.0,
-    tags={ "pg_complexity": 8*1000000 },
-    )
+    tags={"pg_complexity": 8 * 1000000},
+)
 
 register(
     id='RoboschoolHumanoid-v1',
     entry_point='roboschool:RoboschoolHumanoid',
     max_episode_steps=1000,
     reward_threshold=3500.0,
-    tags={ "pg_complexity": 100*1000000 },
-    )
+    tags={"pg_complexity": 100 * 1000000},
+)
 register(
     id='RoboschoolHumanoidFlagrun-v1',
     entry_point='roboschool:RoboschoolHumanoidFlagrun',
     max_episode_steps=1000,
     reward_threshold=2000.0,
-    tags={ "pg_complexity": 200*1000000 },
-    )
+    tags={"pg_complexity": 200 * 1000000},
+)
 register(
     id='RoboschoolHumanoidFlagrunHarder-v1',
     entry_point='roboschool:RoboschoolHumanoidFlagrunHarder',
     max_episode_steps=1000,
-    tags={ "pg_complexity": 300*1000000 },
-    )
+    tags={"pg_complexity": 300 * 1000000},
+)
 
 
 # Atlas
@@ -89,8 +98,8 @@ register(
     id='RoboschoolAtlasForwardWalk-v1',
     entry_point='roboschool:RoboschoolAtlasForwardWalk',
     max_episode_steps=1000,
-    tags={ "pg_complexity": 200*1000000 },
-    )
+    tags={"pg_complexity": 200 * 1000000},
+)
 
 
 # Multiplayer
@@ -99,13 +108,14 @@ register(
     id='RoboschoolPong-v1',
     entry_point='roboschool:RoboschoolPong',
     max_episode_steps=1000,
-    tags={ "pg_complexity": 20*1000000 },
-    )
+    tags={"pg_complexity": 20 * 1000000},
+)
 
 from roboschool.gym_pendulums import RoboschoolInvertedPendulum
 from roboschool.gym_pendulums import RoboschoolInvertedPendulumSwingup
 from roboschool.gym_pendulums import RoboschoolInvertedDoublePendulum
 from roboschool.gym_reacher import RoboschoolReacher
+from roboschool.gym_reacher7d import RoboschoolReacher7d
 from roboschool.gym_mujoco_walkers import RoboschoolHopper
 from roboschool.gym_mujoco_walkers import RoboschoolWalker2d
 from roboschool.gym_mujoco_walkers import RoboschoolHalfCheetah
